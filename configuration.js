096964e4fde66d260dfd62dcdad8fa5185262f9b
fsanti <federica.santicoli@intersail.it>
2015-09-11 13:02:12 +0200

    Added questionaire's instances management, and rearranged files

--- configuration.js
+++ configuration.js
@@ -2,7 +2,7 @@ angular.module("Questionnaire")
     .constant('configuration', {
         //root: '/questionnaires',
         //root: 'file:///Users/mcrescini/Progetti/AngularJS/questionnaires',
-        questionnaires_controller_url: 'http://wrkdev-mcrescini:8000/questionnaires/',
-        questionnaire_templates_controller_url: 'http://wrkdev-mcrescini:8000/questionnaire_templates/',
-        questionnaire_instances_controller_url: 'http://wrkdev-mcrescini:8000/questionnaire_instances/'
+        questionnaires_controller_url: 'http://ced-sisi:8000/questionnaires/',
+        questionnaire_templates_controller_url: 'http://ced-sisi:8000/questionnaire_templates/',
+        questionnaire_instances_controller_url: 'http://ced-sisi:8000/questionnaire_instances/'
     });
