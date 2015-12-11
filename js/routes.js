004d72c6acf70e6c105beaf9d1edf7aedce0202d
mcrescini <michele.crescini@intersail.it>
2015-12-11 15:44:28 +0100

    Directives now specifies templateUrls with a reference to a "questionnaire_root" key defined in configuration

--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,12 @@
 angular.module("Questionnaire")
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', 'configuration', function($routeProvider, configuration) {
         $routeProvider.when('/questionnaires', {
-            templateUrl: 'templates/pages/questionnaires/index.html',
+            templateUrl: configuration.questionnaire_root + 'templates/pages/questionnaires/index.html',
             controller: 'QuestionnairesController',
             controllerAs: 'questionnairesCtrl'
         })
         .when('/questionnaire_instances/:id', {
-            templateUrl: 'templates/pages/questionnaire_instances/edit.html',
+            templateUrl: configuration.questionnaire_root + 'templates/pages/questionnaire_instances/edit.html',
             controller: 'QuestionnaireInstancesController',
             controllerAs: 'questionnaireInstancesCtrl'
         })
